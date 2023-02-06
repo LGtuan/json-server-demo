de467d75d164bce9694c8178076decf23d69b24d
origin <tuanlgph20769>
2023-02-06 16:42:01 +0700

    first commit

---
+++ generate.js
@@ -0,0 +1,22 @@
+
+// import { faker } from '@faker-js/faker/locale/de';
+const {faker} = require('@faker-js/faker')
+const USERS = [];
+
+function createRandomUser() {
+  return {
+    // userId: faker.datatype.uuid(),
+    // username: faker.internet.userName(),
+    // email: faker.internet.email(),
+    // avatar: faker.image.avatar(),
+    // password: faker.internet.password(),
+    // birthdate: faker.date.birthdate(),
+    // registeredAt: faker.date.past(),
+  };
+}
+
+Array.from({ length: 10 }).forEach(() => {
+//   USERS.push(createRandomUser());
+    console.log(createRandomUser());
+    console.log(faker)
+});
